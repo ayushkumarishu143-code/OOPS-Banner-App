ccaca309c700c451125dc1e3e3069e7f1b3d4db7
ayushkumarishu143 <ayushkumar.ishu143@gmail.com>
2026-02-23 00:10:06 +0530

    Fixed UC1 to print literal OOPS only

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,14 @@
 /**
  * OOPSBannerApp
- * UC2 - Print OOPS Banner
+ * UC1 - Print literal text "OOPS"
  *
  * @author Ishu
- * @version 2.0
+ * @version 1.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("OOPS");
     }
 }
